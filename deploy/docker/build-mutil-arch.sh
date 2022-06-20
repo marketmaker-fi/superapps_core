57f8c0ed29066ec3bfeb7e641cd812d532e1b426
aisuperapps <106729775+aisuperapps@users.noreply.github.com>
2022-06-20 21:35:27 +1200

    deploy scripts and images

--- deploy/docker/build-mutil-arch.sh
+++ deploy/docker/build-mutil-arch.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker buildx build --platform linux/arm64,linux/amd64 --push -t appsmith/appsmith-ce  .
+#docker buildx build --platform linux/arm64,linux/amd64 --push -t appsmith/appsmith-ce  .
+docker buildx build --platform linux/arm64,linux/amd64 --push -t aisuperapps/superapps  .
